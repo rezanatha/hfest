b0dacea57262db171e70dc7e591312c7cf639df3
rezanatha <36593988+rezanatha@users.noreply.github.com>
2025-04-22 01:03:46 +0700

    create logic on estimate-resource

--- src/hfest/commands/estimate_resource.py
+++ src/hfest/commands/estimate_resource.py
@@ -1,18 +1,90 @@
+from commands.estimate_size import _estimate_model_files
+import subprocess
+
+def get_gpu_info():
+    try:
+        # Run nvidia-smi command
+        output = subprocess.check_output(['nvidia-smi', 
+                                          '--query-gpu=index,name,memory.total,memory.used,memory.free', '--format=csv,noheader'], 
+                                        universal_newlines=True)
+        
+        # Process the output
+        gpu_info = []
+        for line in output.strip().split('\n'):
+            values = [x.strip() for x in line.split(',')]
+            gpu_info.append({
+                'index': values[0],
+                'name': values[1],
+                'memory.total': values[2],
+                'memory.used': values[3],
+                'memory.free': values[4]
+            })
+            
+        return gpu_info
+    except:
+        return "No NVIDIA GPUs detected or nvidia-smi failed to run"
+
+def compare_single_setup(estimated_total, gpu_info):
+    # how many resources would it take?
+    size_mb = int(estimated_total['safetensors'] / (1024 * 1024))
+    for gpu in gpu_info:
+        gpu_mb = int(gpu['memory.free'].split(" ")[0])
+        if size_mb > gpu_mb:
+            print(f"Insufficient memory on GPU {gpu['index']}: {gpu['name']}: Model size {size_mb} MB vs Free GPU memory {gpu_mb} MB")
+        else:
+            print(f"Model would fit on GPU {gpu['index']}: {gpu['name']}: Model size {size_mb:2f} MB vs Free GPU memory {gpu_mb} MB")
+
+def compare_quantized(estimated_total, gpu_info):
+    return 0
+
+def compare_distributed(estimated_total, gpu_info):
+    return 0
+
+
 def setup_parser(subparsers):
     parser = subparsers.add_parser("estimate-resource", help = "Estimate model size and resource needed to run the model")
     parser.add_argument("model_id", help="Hugging Face model ID (e.g., meta-llama/Llama-2-7b)")
-    parser.add_argument("gpu_config", help="GPU config the model is running on (0: auto, 1: single setup, 2: distributed setup, )")
-    parser.add_argument("quantization", help="level of quantization (0: auto, 1: minimal, 2: adequate, 3: aggresive)")
+    parser.add_argument("--gpu_config", type=int, default=0, help="GPU config the model is running on (0: auto, 1: single setup, 2: distributed setup, )")
+    parser.add_argument("--quantization", type=int, default=0, help="level of quantization (0: auto, 1: minimal, 2: adequate, 3: aggresive)")
     return parser
 
 def handle(args):
     print(f"Estimating resources for model: {args.model_id}")
-    print("handle_estimate_resource not implemented")
     # estimate model size
+    estimated_total = _estimate_model_files(args)
     # detect host GPU specifications (from something like nvidia-smi)
+    # detect nvidia GPU
+    gpu_info = get_gpu_info()
+    if isinstance(gpu_info, list):
+        print(f"Number of available GPUs: {len(gpu_info)}")
+        for gpu in gpu_info:
+            print(f"GPU {gpu['index']}: {gpu['name']}")
+            print(f"  Total memory: {gpu['memory.total']}")
+            print(f"  Used memory: {gpu['memory.used']}")
+            print(f"  Free memory: {gpu['memory.free']}")
+    else:
+        print(gpu_info)
+
     # compare gpu spec with model size, is it possible to run on it?
-    # how many resources would it take?
-    # if not quantized/quantized (4FP, 16FP, etc)
-    # if running on a single/distributed system
-    # provide recommendations
+    # MODEL PRIORITY
+    # 1. SAFETENSORS
+    if 'safetensors' in estimated_total:
+        # Original Settings, all model is fitted into GPU
+        print("Comparing model size and free GPU memory on single GPU settings")
+        compare_single_setup(estimated_total, gpu_info)
+        # IF QUANTIZED 
+        # IF SHARDED AND DISTRIBUTED
+        # if running on a single/distributed system
+
+    # 2. PYTORCH BIN
+    elif 'pytorch' in estimated_total:
+        print("Comparing model size and free GPU memory on single GPU settings")
+        compare_model_and_gpu(estimated_total, gpu_info)
+    # 3. ONNX
+    elif 'onnx' in estimated_total:
+        print("Comparing model size and free GPU memory on single GPU settings")
+        compare_model_and_gpu(estimated_total, gpu_info)
+    # 4. OTHERS
+
+    
     return 0   
--- src/hfest/commands/estimate_size.py
+++ src/hfest/commands/estimate_size.py
@@ -1,14 +1,18 @@
 from utils.config import read_config
 from huggingface_hub import hf_hub_download, scan_cache_dir, HfApi, login
 import json
+import requests
 
 def setup_parser(subparsers):
     parser = subparsers.add_parser("estimate-size", help="Estimate model size")
     parser.add_argument("model_id", help="Hugging Face model ID (e.g., meta-llama/Llama-2-7b)")
     return parser
 
-def handle(args):
-    print(f"Estimating size for model: {args.model_id}")
+# TODO:
+# validate the input from args.model_id: it should only process model ID like account/model_name
+
+def _estimate_model_files(args):
+
     config = read_config()
 
     if config['api_key'] is None:
@@ -18,7 +22,7 @@ def handle(args):
     # Initialize the API
     api = HfApi()
     login(config['api_key'])
-    import requests
+
     response = requests.get(
         f"https://huggingface.co/api/models/{args.model_id}",
         params={'fields': ['usedStorage', 'safetensors', 'siblings']},
@@ -27,6 +31,7 @@ def handle(args):
     total_used_storage = None
     model_params_size = None
     repo_files = None
+
     if response.status_code == 200:
         content = json.loads(response.content)
         repo_files = content.get('siblings', None)
@@ -38,45 +43,56 @@ def handle(args):
 
     model_extensions = (('safetensors',['safetensors']), ('pytorch', ['bin', 'pt', 'pth']))
     model_files = {k[0]: [] for k in model_extensions}
-    for r in repo_files:
-        for k, v in model_extensions:
-            if r['rfilename'].split('.')[-1] in v:
-                model_files[k].append(r['rfilename'])
+    
+    if isinstance(repo_files, list):
+        for r in repo_files:
+            for k, v in model_extensions:
+                if r['rfilename'].split('.')[-1] in v:
+                    model_files[k].append(r['rfilename'])
+    else:
+        print("is an empty repository")
+        return None
+            
     print("Model files: ")
     for k,v in model_files.items():
         print(f"{len(v)} {k}",end=" ")
     print("\n")
 
-    # # connect to model ID
-    # model_id = args.model_id
-    # model_files = api.list_repo_files(model_id)
-
-    # model_files = [f for f in model_files if any(f.endswith(ext) for ext in model_extensions)]
-    # print(f"got {len(model_files)} model files, only print the first 5")
-    
     # fetch real model size 
+    estimated_total = {k[0]: [] for k in model_extensions}
     for model_type, model_name in model_files.items():
         file_infos = []
         for file in model_name[:5]:  # Limit to first 5 files to avoid API abuse
             file_info = api.get_paths_info(repo_id=args.model_id, paths=[file])[0]
             file_infos.append((file, file_info.size if hasattr(file_info, 'size') and file_info.size else "Unknown"))
 
-        for file, size in file_infos:
-            if size != "Unknown":
-                size_mb = size / (1024 * 1024)
-                print(f"{model_type} File: {file}, Size: {size_mb:.2f} MB")
-            else:
-                print(f"{model_type} File: {file}, Size: Unknown")
+        # for file, size in file_infos:
+        #     if size != "Unknown":
+        #         size_mb = size / (1024 * 1024)
+        #         print(f"{model_type} File: {file}, Size: {size_mb:.2f} MB")
+        #     else:
+        #         print(f"{model_type} File: {file}, Size: Unknown")
         
         # For models with many shards, we can estimate total size in GB based on a sample of 5 models
         if any(size != "Unknown" for _, size in file_infos):
             known_sizes = [size for _, size in file_infos if size != "Unknown"]
             if known_sizes:
                 avg_size = sum(known_sizes) / len(known_sizes)
-                estimated_total = avg_size * len(model_name)
-                print(f"Estimated used storage for {model_type}: {estimated_total / (1024**3):.2f} GB")
+                estimated_total[model_type] = avg_size * len(model_name)
+                print(f"Estimated used storage for {model_type}: {estimated_total[model_type] / (1024**3):.2f} GB")
             else:
                 print("Model size unknown")
 
+    return estimated_total
+
+def handle(args):
+    # # connect to model ID
+    # model_id = args.model_id
+    # model_files = api.list_repo_files(model_id)
+
+    # model_files = [f for f in model_files if any(f.endswith(ext) for ext in model_extensions)]
+    # print(f"got {len(model_files)} model files, only print the first 5")
+    print(f"Estimating size for model: {args.model_id}")
+    estimated_total = _estimate_model_files(args)
     
     return 0
