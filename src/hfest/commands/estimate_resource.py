3e453f089a2e4fa0cd19100ffecf960cc903174c
rezanatha <36593988+rezanatha@users.noreply.github.com>
2025-04-24 00:24:03 +0700

    rework prints

--- src/hfest/commands/estimate_resource.py
+++ src/hfest/commands/estimate_resource.py
@@ -1,6 +1,11 @@
 from .estimate_size import estimate_model_files
 import subprocess
 
+RED = "\033[91m"
+GREEN = "\033[92m"
+YELLOW = "\033[93m"
+RESET = "\033[0m"  # Reset color to terminal default
+
 def get_gpu_info():
     try:
         # Run nvidia-smi command
@@ -26,13 +31,13 @@ def get_gpu_info():
 
 def compare_single_setup(estimated_total, gpu_info, margin_of_safety = 0.2):
     # how many resources would it take?
-    size_mb = int(estimated_total['safetensors'] / (1024 * 1024))
+    size = estimated_total['safetensors'] / (1024 ** 3)
     for gpu in gpu_info:
-        gpu_mb = int(gpu['memory.free'].split(" ")[0])
-        if size_mb + size_mb * margin_of_safety > gpu_mb :
-            print(f"Insufficient memory on GPU {gpu['index']}: {gpu['name']}: Model size {size_mb} MB vs Free GPU memory {gpu_mb} MB")
+        gpu_free = float(gpu['memory.free'].split(" ")[0]) / 1024 
+        if size + size * margin_of_safety > gpu_free:
+            print(f"  • {RED}[NOT ENOUGH MEMORY]{RESET} on GPU {gpu['index']}: {gpu['name']}. Model size {size:.2f} GB vs Free GPU memory {gpu_free:.2f} GB")
         else:
-            print(f"Model would fit on GPU {gpu['index']}: {gpu['name']}: Model size {size_mb} MB vs Free GPU memory {gpu_mb} MB")
+            print(f"  • {GREEN}[MEMORY CHECK PASSED]{RESET} for {gpu['index']}: {gpu['name']}. Model size {size:.2f} GB vs Free GPU memory {gpu_free:.2f} GB")
 
 def compare_quantized(estimated_total, gpu_info):
     return 0
@@ -49,28 +54,33 @@ def setup_parser(subparsers):
     return parser
 
 def handle(args):
-    print(f"Estimating resources for model: {args.model_id}")
+    print(f"Model: {args.model_id}")
+    print("----------------------------------------")
     # estimate model size
     estimated_total = estimate_model_files(args)
+    print("----------------------------------------")
     # detect host GPU specifications (from something like nvidia-smi)
     # detect nvidia GPU
     gpu_info = get_gpu_info()
     if isinstance(gpu_info, list):
         print(f"Number of available GPUs: {len(gpu_info)}")
         for gpu in gpu_info:
-            print(f"GPU {gpu['index']}: {gpu['name']}")
-            print(f"  Total memory: {gpu['memory.total']}")
-            print(f"  Used memory: {gpu['memory.used']}")
-            print(f"  Free memory: {gpu['memory.free']}")
+            free = int(gpu['memory.free'].split(" ")[0])
+            total = int(gpu['memory.total'].split(" ")[0])
+            print(f"  • GPU {gpu['index']}: {gpu['name']} ({total - free}/{total} MB)")
+            # print(f"  • Total memory: {gpu['memory.total']}")
+            # print(f"  • Used memory: {gpu['memory.used']}")
+            # print(f"  • Free memory: {gpu['memory.free']}")
     else:
         print(gpu_info)
 
     # compare gpu spec with model size, is it possible to run on it?
     # MODEL PRIORITY
     # 1. SAFETENSORS
+    print("----------------------------------------")
     if 'safetensors' in estimated_total:
         # Original Settings, all model is fitted into GPU
-        print("Comparing model size and free GPU memory on single GPU settings")
+        print("[SINGLE GPU] Model File Size vs Free GPU Memory:")
         compare_single_setup(estimated_total, gpu_info)
         # IF QUANTIZED 
         # IF SHARDED AND DISTRIBUTED
